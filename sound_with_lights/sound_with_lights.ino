08ee01ed53719d2fc493157b710a32827532cefe
bartulem <mimica.bartul@gmail.com>
2022-07-21 15:37:31 -0400

    add sound_with_lights.ino 072122v02

--- sound_with_lights/sound_with_lights.ino
+++ sound_with_lights/sound_with_lights.ino
@@ -6,41 +6,43 @@ This sketch generates square wave tones with blinking LED output.
 @bartulem 2022
 */
 
-#define output_pin 13
-#define speaker 3
+#define output_pin 7
+#define speaker 13
 #define N_PINS 3
 const int pins[N_PINS] = {2, 3, 4};
-int tone_duration = 2;
 int freq = 800;
-int initial_delay = 10000;
-int ipi_delay = 5000;
-unsigned long start_time;
+int initial_delay = 2000;
+int ipi_delay = 3000;
+int tone_delay = 2000; 
+unsigned long on_time;
+unsigned long off_time;
 
 void setup()
 {
   Serial.begin(115200);
-  pinMode(speaker, OUTPUT);
   for(int i=0; i<N_PINS; i++) {
     pinMode(pins[i], OUTPUT);
     digitalWrite(pins[i], LOW);
   }
+  pinMode(speaker, OUTPUT);
+  pinMode(output_pin, OUTPUT);
+  digitalWrite(output_pin, LOW);
   delay(initial_delay);
 }
 
 void loop()
 {
-  start_time = millis();
-  if( (millis() - start_time) <= tone_duration*1000){
-    tone(speaker, freq);
-    for(int i=0; i<N_PINS; i++) {
-      digitalWrite(pins[i], HIGH);
-    }
+  on_time = millis();
+  while (millis()-on_time <= tone_delay) {
+    for(int i=0; i<N_PINS; i++){digitalWrite(pins[i], HIGH);}
     digitalWrite(output_pin, HIGH);
+    tone(speaker, freq);
   }
-  for(int i=0; i<N_PINS; i++) {
-    digitalWrite(pins[i], LOW);
-  }
-  digitalWrite(output_pin, LOW);
   
-delay(ipi_delay);
+  off_time = millis();
+  while (millis()-off_time <= ipi_delay) {
+    for(int i=0; i<N_PINS; i++){digitalWrite(pins[i], LOW);}
+    digitalWrite(output_pin, LOW);
+    noTone(speaker);
+    } 
 }
